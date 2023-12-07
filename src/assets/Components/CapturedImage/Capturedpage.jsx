c0660a2b969c46886c78eb4af7344aa6acee90a5
ewi josepha <ewijosepha@gmail.com>
2023-12-07 15:04:30 +0100

    first page implementation

--- src/assets/Components/CapturedImage/Capturedpage.jsx
+++ src/assets/Components/CapturedImage/Capturedpage.jsx
@@ -14,12 +14,27 @@ function Capturedpage() {
             <p>...</p>
           </div>
           <div className="rightheaders">
-            <i class="fa-solid fa-battery-three-quarters"></i>
             <i class="fa-solid fa-wifi"></i>
+            <i class="fa-solid fa-battery-three-quarters"></i>
           </div>
         </div>
         <div className="bgimage">
           <img src="/src/assets/images/index.jpeg" />
+        </div>
+        <div className="flags">
+          <div className="flag1">
+            <img src=""/>
+            <p>welcome</p>
+          </div>
+          <div className="scores">
+            <h5>1</h5>
+            <h5>-</h5>
+            <h5>1</h5>
+          </div>
+          <div className="flag2">
+            <img src="" />
+            <p>welcome</p>
+          </div>
         </div>
       </div>
     </>
