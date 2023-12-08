201024d6af9637c6e0601ebc79fcb4c442ee05ff
ewi josepha <ewijosepha@gmail.com>
2023-12-08 13:26:50 +0100

    implementing scores

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import './App.css'
 import Selectedclubs from './assets/Components/Selectclubs/Selectedclubs'
 import Capturedpage from './assets/Components/CapturedImage/Capturedpage'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Appcontextt } from './assets/Components/Hooks/context'
 
 function App() {
   const client = new QueryClient()
+  const [score, setScore] = useState(0)
 
   return (
     <>
+    <Appcontextt.Provider value={{score, setScore}}>
     <QueryClientProvider client={client}>
       {/* <Capturedpage /> */}
       <Selectedclubs/>
     </QueryClientProvider>
+    </Appcontextt.Provider>
   
     </>
   )
--- src/assets/Components/Selectclubs/Selectedclubs.jsx
+++ src/assets/Components/Selectclubs/Selectedclubs.jsx
@@ -1,43 +1,36 @@
 import { useState } from "react";
 import { jsonObject } from "../Json/Json";
-function Selectclubs () {
-  const [score, setScore]= useState(0)
-  const [showinput, setShowinput]= useState(false)
-  const clubs = jsonObject.clubs
-function show () {
-  setShowinput(!showinput)
-  console.log("hey");
-}
+import { Appcontextt } from "../Hooks/context";
+import { useContext } from "react";
+function Selectclubs() {
+  const [score, setScore] = useContext(Appcontextt) 
+  const clubs = jsonObject.clubs;
 
-function handlescores (e) {
-  setScore(e.target.value)
-}
+  function handlescores() {
+    const randomScore = Math.floor(Math.random() * 10)
+    setScore(randomScore)
+ 
+  }
 
-console.log(score);
+  console.log(score);
 
-
-  return(
+  return (
     <>
-  <div className="body1">
-    <div className="header1">
-      <h1 id="title"> Click On Any Flag to Forcast the Score </h1>
-    </div>
-    {clubs.slice(0,100).map((clubs)=>(
-        <div className="flagg">
-         
-        <img src={clubs.url} onClick={show} />
-        <div className="input">
-        <h5>{clubs.name}</h5>
-        {show &&
-        <input type="text"  id="inputfield" onChange={handlescores}/>}
+      <div className="body1">
+        <div className="header1">
+          <h1 id="title"> Click On Any Flag to Forcast the Score </h1>
         </div>
+        {clubs.slice(0, 100).map((clubs, index) => (
+          <div className="flagg">
+            <img src={clubs.url} key={index} onClick={handlescores} />
+            <div className="input">
+              <h5>{clubs.name}</h5>
+            </div>
+          </div>
+        ))}
       </div>
-    ))}
-  
-  </div>
     </>
-  )
-
+  );
 }
 
-export default Selectclubs
+export default Selectclubs;
